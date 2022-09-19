8d65f46552f1bdb00940855e47ae5fdd61f660e9
Herbert Lin <herbert.lin.7@gmail.com>
2022-09-19 14:29:12 -0500

    Check if category exists before insert

--- backend-express-mongodb/src/fetchData.ts
+++ backend-express-mongodb/src/fetchData.ts
@@ -18,15 +18,25 @@ interface ICategory {
 const getAndSaveCategoriesData = async () => {
   const url = `${BASE_URL}/us/categories?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_APP_KEY}`;
   const response = await axios.get(url);
-  console.log(response.data.results);
+  // console.log(response.data.results);
   return mongoose.connect(`${process.env.DB_HOST_DOCKER}`)
-    .then(() => {
-      return Category.insertMany(response.data.results.map((elem: ICategory) => {
-        return {
-          tag: elem.tag,
-          label: elem.label
-        };
+    .then(async () => {
+      const dataExistCheck = await Promise.all(response.data.results.map(async (elem: ICategory) => {
+        const data = await Category.find({ tag: elem.tag });
+        return data.length === 0;
       }));
+      const newData = response.data.results.filter((elem: ICategory, index: number) => {
+        return dataExistCheck[index];
+      })
+        .map((elem: ICategory) => {
+          return {
+            tag: elem.tag,
+            label: elem.label
+          };
+        });
+      if (newData.length > 0) {
+        return Category.insertMany(newData);
+      }
     })
     .catch((err: any) => {
       console.log(err);
