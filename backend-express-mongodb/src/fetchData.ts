0f78d70447b404e0f56c96b4d72ab6ed037600c1
Herbert Lin <herbert.lin.7@gmail.com>
2022-09-20 11:34:56 -0500

    Update fetch data script

--- backend-express-mongodb/src/fetchData.ts
+++ backend-express-mongodb/src/fetchData.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import axios from 'axios';
 import mongoose from 'mongoose';
 import Category from './models/Category';
+import Historical from './models/Historical';
+import Histogram from './models/Histogram';
 
 dotenv.config();
 
@@ -15,33 +17,107 @@ interface ICategory {
   label: string;
 }
 
+const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+
 const getAndSaveCategoriesData = async () => {
   const url = `${BASE_URL}/us/categories?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_APP_KEY}`;
   const response = await axios.get(url);
   // console.log(response.data.results);
-  return mongoose.connect(`${process.env.DB_HOST_DOCKER}`)
-    .then(async () => {
-      const dataExistCheck = await Promise.all(response.data.results.map(async (elem: ICategory) => {
-        const data = await Category.find({ tag: elem.tag });
-        return data.length === 0;
-      }));
-      const newData = response.data.results.filter((elem: ICategory, index: number) => {
-        return dataExistCheck[index];
-      })
-        .map((elem: ICategory) => {
-          return {
-            tag: elem.tag,
-            label: elem.label
-          };
-        });
-      if (newData.length > 0) {
-        return Category.insertMany(newData);
+  const newData = [];
+  const fetchedCategories = response.data.results;
+  for (let i = 0; i < fetchedCategories.length; i += 1) {
+    const existingCategory = await Category.findOne({ tag: fetchedCategories[i].tag });
+    if (existingCategory) {
+      if (existingCategory.label !== fetchedCategories[i].label) {
+        existingCategory.label = fetchedCategories[i].label;
+        existingCategory.save();
       }
-    })
-    .catch((err: any) => {
-      console.log(err);
-    });
+    } else {
+      newData.push({
+        tag: fetchedCategories[i].tag,
+        label: fetchedCategories[i].label
+      })
+    }
+  }
+  if (newData.length > 0) {
+    Category.insertMany(newData);
+  }
+};
+
+const getAllCategories = async () => {
+  const categoriesRaw = await Category.find();
+  return categoriesRaw.map(elem => elem.tag);
 }
 
-getAndSaveCategoriesData()
-  .then((res) => console.log(res));
+const getAndSaveHistoricalData = async (categories: string[]) => {
+  const getHistoricalRawArr = async () => {
+    const result = [];
+    for (let i = 0; i < categories.length; i += 1) {
+      await delay(3000);
+      const url = `${BASE_URL}/us/history?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_APP_KEY}&category=${categories[i]}`;
+      const response = await axios.get(url);
+      result.push(response.data);
+    }
+    return result;
+  }
+
+  const historicalRawArr = await getHistoricalRawArr();
+  for (let i = 0; i < historicalRawArr.length; i += 1) {
+    await delay();
+    const existingHistorical = await Historical.findOne({ category: categories[i] });
+    if (!existingHistorical) {
+      const historical = new Historical({
+        category: categories[i],
+        month: historicalRawArr[i].month
+      });
+      historical.save();
+    } else {
+      existingHistorical.month = historicalRawArr[i].month;
+      existingHistorical.save();
+    }
+  }
+};
+
+const getAndSaveHistogramData = async (categories: string[]) => {
+  const getHistogramRawArr = async () => {
+    const result = [];
+    for (let i = 0; i < categories.length; i += 1) {
+      await delay(3000);
+      const url = `${BASE_URL}/us/histogram?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_APP_KEY}&category=${categories[i]}`;
+      const response = await axios.get(url);
+      result.push(response.data);
+    }
+    return result;
+  }
+
+  const histogramRawArr = await getHistogramRawArr();
+  for (let i = 0; i < histogramRawArr.length; i += 1) {
+    await delay();
+    const existingHistogram = await Histogram.findOne({ category: categories[i] });
+    if (!existingHistogram) {
+      const historical = new Histogram({
+        category: categories[i],
+        histogram: histogramRawArr[i].histogram
+      });
+      historical.save();
+    } else {
+      existingHistogram.histogram = histogramRawArr[i].histogram;
+      existingHistogram.save();
+    }
+  }
+};
+
+(async () => {
+  try {
+    const dbConnection = await mongoose.connect(`${process.env.DB_HOST_DOCKER}`);
+    
+    await getAndSaveCategoriesData();
+    const categories = await getAllCategories();
+    await getAndSaveHistoricalData(categories);
+    await getAndSaveHistogramData(categories);
+    
+    dbConnection.disconnect();
+  } catch (err) {
+    console.log(err);
+  }
+})();
