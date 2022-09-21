799e46f9b0216fb5b602954cb05ed5b9e33ea470
Herbert Lin <herbert.lin.7@gmail.com>
2022-09-21 09:44:31 -0500

    Fix controllers not responding

--- backend-express-mongodb/src/controllers/Admin.ts
+++ backend-express-mongodb/src/controllers/Admin.ts
@@ -19,15 +19,7 @@ export const getCategories = (req: Request, res: Response): void => {
 export const getCategory = async (req: Request, res: Response): Promise<any> => {
   const categoryTag = req.params.categoryTag;
 
-  console.log('Category tag: ', categoryTag);
-
-  // This line of code fixes the CastError: Cast to ObjectId failed for value "favicon.ico" (type string) at path "_id" for model "contents"
-
-  if (!mongoose.Types.ObjectId.isValid(categoryTag)) return false;
-
-  // await Category.findById(categoryTag).exec();
-
-  Category.findById(categoryTag, (err: any, doc: any) => {
+  Category.findOne({ tag: categoryTag }, (err: any, doc: any) => {
       console.log(doc);
 
       res.json(doc);
@@ -41,17 +33,10 @@ export const getCategory = async (req: Request, res: Response): Promise<any> => 
 export const getHistoricalByCategory = async (req: Request, res: Response): Promise<any> => {
   const categoryTag = req.params.categoryTag;
 
-  console.log('Category tag: ', categoryTag);
-
-  // This line of code fixes the CastError: Cast to ObjectId failed for value "favicon.ico" (type string) at path "_id" for model "contents"
-
-  if (!mongoose.Types.ObjectId.isValid(categoryTag)) return false;
-
   Historical.findOne({ category: categoryTag }, (err: any, doc: any) => {
     console.log(doc);
 
-    const parsedHistorical = Object.entries(doc.month)
-      .map(([key, value]) => ({ month: Date.parse(key), average: value}))
+    const parsedHistorical = Array.from(doc.month, ([key, value]) => ({ month: Date.parse(key), average: value}))
       .sort((a, b) => {
         return a.month - b.month;
       });
@@ -67,17 +52,10 @@ export const getHistoricalByCategory = async (req: Request, res: Response): Prom
 export const getHistogramByCategory = async (req: Request, res: Response): Promise<any> => {
   const categoryTag = req.params.categoryTag;
 
-  console.log('Category tag: ', categoryTag);
-
-  // This line of code fixes the CastError: Cast to ObjectId failed for value "favicon.ico" (type string) at path "_id" for model "contents"
-
-  if (!mongoose.Types.ObjectId.isValid(categoryTag)) return false;
-
   Histogram.findOne({ category: categoryTag }, (err: any, doc: any) => {
     console.log(doc);
 
-    const parsedHistogram = Object.entries(doc.histogram)
-      .map(([key, value]) => ({ bin: key, amount: value}))
+    const parsedHistogram = Array.from(doc.histogram, ([key, value]) => ({ bin: key, amount: value}))
       .sort((a, b) => +a.bin - +b.bin);
     
     res.json(parsedHistogram);
