1a3c8b8f097428660ccbd5a6d17cae6cc373892f
Herbert Lin <herbert.lin.7@gmail.com>
2022-09-26 13:45:46 -0500

    Update files to load from --env-file correctly

--- backend-express-mongodb/src/app.ts
+++ backend-express-mongodb/src/app.ts
@@ -1,14 +1,4 @@
-// import dotenv from 'dotenv';
-
-// const result = dotenv.config()
-
-// if (result.error) {
-//   throw result.error
-// }
-
-// // console.log(result.parsed);
-
-// console.log(process.env.DB_HOST);
+console.log(process.env);
 
 import express from 'express';
 
